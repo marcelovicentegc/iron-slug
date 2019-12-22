c3d4f98f322f25766d687f33587c3c2cdba2dc71
Marcelo Cardoso <marcelovicentegc@gmail.com>
2019-12-22 00:41:24 -0300

    feature: running, crouching, aiming up and shooting animations

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -7,20 +7,23 @@ public class Player : MonoBehaviour {
 
 	public float speed = 5f;
 	public float jumpForce = 600;
-
+	private Animator animator;
 	private Rigidbody2D rb2d;
 	private bool isFacingRight = true;
 	private bool jump;
 	private bool isOnLand = false;
 	private Transform landCheck;
-
 	private float xForce = 0;
 	private bool isDead = false;
+	private bool isCrouched;
+	private bool isAimingUp;
+	private bool isReloading;
 
 	// Use this for initialization
 	void Start () {
 		rb2d = GetComponent<Rigidbody2D>();
 		landCheck = gameObject.transform.Find("LandCheck");
+		animator = GetComponent<Animator>();
 	}
 	
 	// Update is called once per frame
@@ -28,19 +31,47 @@ public class Player : MonoBehaviour {
 		if (!isDead) {
 			isOnLand = Physics2D.Linecast(transform.position, landCheck.position, 1 << LayerMask.NameToLayer("Land"));
 
-			if (Input.GetButtonDown("Jump") && isOnLand) {
+			if (isOnLand) {
+				animator.SetBool("Jump", false);
+			}
+
+			if (Input.GetButtonDown("Jump") && isOnLand && !isReloading) {
 				jump = true;
 			} else if (Input.GetButtonUp("Jump")) {
 				if (rb2d.velocity.y > 0) {
 					rb2d.velocity = new Vector2(rb2d.velocity.x, Convert.ToSingle(rb2d.velocity.y * 0.5));
 				};
 			}
+
+			if (Input.GetButtonDown("Fire1")) {
+				animator.SetTrigger("Shoot");
+			}
+
+			isAimingUp = Input.GetButton("Up");
+			isCrouched = Input.GetButton("Down");
+
+			animator.SetBool("AimUp", isAimingUp);
+			animator.SetBool("Crouch", isCrouched);
+
+			if (Input.GetButtonDown("Reload")) {
+				animator.SetBool("Reload", true);
+			} else {
+				animator.SetBool("Reload", false);
+			}
+
+			if ((isCrouched || isAimingUp || isReloading) && isOnLand) {
+				xForce = 0;
+			}
 		}
 	}
 
 	private void FixedUpdate () {
 		if (!isDead) {
-			xForce = Input.GetAxisRaw("Horizontal");
+			if (!isCrouched && !isAimingUp && !isReloading) {
+				xForce = Input.GetAxisRaw("Horizontal");
+			}
+
+			animator.SetFloat("Speed", Mathf.Abs(xForce));
 
 			rb2d.velocity = new Vector2(xForce * speed, rb2d.velocity.y);
 
@@ -51,6 +82,7 @@ public class Player : MonoBehaviour {
 			}
 
 			if (jump) {
+				animator.SetBool("Jump", true);
 				jump = false;
 				rb2d.AddForce(Vector2.up * jumpForce);
 			}
